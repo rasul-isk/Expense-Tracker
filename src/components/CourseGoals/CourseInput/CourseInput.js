5c6c5303449ffa0c4118296e09b07c8ebb01fb08
Rasul Isk <rasul_iskenderov@mail.ru>
2022-11-26 09:45:35 +0300

    Intoruduction to styled components library

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -3,21 +3,27 @@ import React, { useState } from 'react';
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
-const CourseInput = props => {
+const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
-
-  const goalInputChangeHandler = event => {
+  const [isValid, setIsValid] = useState(true);
+  const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
+    if (event.target.value.trim().length != 0) setIsValid(true);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
